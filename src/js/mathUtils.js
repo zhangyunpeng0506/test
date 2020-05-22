d15100e372cbd5d6b452ba647c87611f803e48d3
zhangyunpeng0506 <zhangyunpeng0506@163.com>
2020-05-22 15:35:26 +0800

    webpack配置练习

---
+++ src/js/mathUtils.js
@@ -0,0 +1,17 @@
+//数组求和
+export function sum(...args){
+  if(args.every(val=>val&&!isNaN(val))){
+    return args.reduce((prev,current)=>prev+current)
+  }else{
+    console.log('包含非数字');
+  }
+}
+
+//数组去重
+export function unique(args){
+  if(Array.isArray(args)){
+    return Array.from(new Set(args));
+  }else{
+    console.log('不是数组');
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import * as mathUtils from './js/mathUtils'
+
+console.log(mathUtils.sum(1));
+
+require('./css/normal.css');
