5957ce62a2b9a6661397fa55fab3801b20b269da
zhangyunpeng0506 <zhangyunpeng0506@163.com>
2020-05-24 23:31:08 +0800

    createElement

--- src/main.js
+++ src/main.js
@@ -17,8 +17,26 @@ import App from './vue/App.vue'
 
 new Vue({
   el: '#app',
-  template: `<App></App>`,
-  components: {
-    App,
-  }
+  // template: `<App></App>`,
+  // components: {
+  //   App,
+  // }
+  render: h => h(App)
+  // render: h => h('h2',
+  //   { class: 'title' },
+  //   ['你好呀', h('p', { class: 'content' }), ['我是内容']])
 })
+
+
+/*
+  runtime-compiler()
+  template -> ast(抽象语法树) -> render -> v-dom -> ui
+
+  runtime-only()  1.性能更高；2.代码量少；3.vue中的template已经由vue-template-compiler编译了
+  render -> vdom ->ui
+
+  //1.普通用法
+  creatElement('标签',{标签的属性},['标签的内容'])
+  //2.传入组件对象
+  creatElement(App)
+*/
